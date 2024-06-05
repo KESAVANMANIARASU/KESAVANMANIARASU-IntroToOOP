bee00d52a4ff9a85a49d3d6225b1be8a816f5a60
KESAVANMANIARASU <kesavanm0904@gmail.com>
2024-06-05 21:39:02 +0530

    commit

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -27,7 +27,7 @@
  * 
  * 
  */
-public class Bird {
+public class Bird{
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
@@ -44,8 +44,19 @@ public class Bird {
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
-    public String sound(){
-        return "";
+    public String sound() {
+        if (ageMonths<3){
+           return "Tweet";
+        }
+        else if (ageMonths>=3){
+           return "Chirp";
+        }
+        else{
+            return " ";
+        }
+
+
+        
     }
 
 }
